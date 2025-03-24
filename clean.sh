2829f8b23ace12f745e0985b819780723ec9263a
Evgenii Sopov <mrseakg@gmail.com>
2025-03-24 18:40:56 +0700

    Updated

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf data/logs/*.log
+rm -rf data/db
+rm -rf data/checker_bigu/*_BioGuard.db
+rm -rf data/checker_bigu/__pycache__
+rm -rf data/checker_eyesee/__pycache__
+rm -rf data/checker_bigu/*_MSPD.db
+rm -rf data/checker_neli234/*_MSPD.db
+rm -rf data/checker_unpleasant/__pycache__
---
+++ fix_chmod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod 775 data/checker_bank
+chmod 775 data/checker_bigu
+chmod 775 data/checker_eyesee
+chmod 775 data/checker_mspd
+chmod 775 data/checker_ncdex
+chmod 775 data/checker_neli234
+chmod 775 data/checker_unpleasant
+chmod 775 data/checker_wrnum
